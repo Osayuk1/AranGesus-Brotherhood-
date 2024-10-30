fb1d57bb13585bb610d57a21f5bd57327d7b3057
Osayuk1 <157780677+Osayuk1@users.noreply.github.com>
2024-10-30 19:45:28 +0100

    Update calculator_for_buyers.js

--- calculator_for_buyers.js
+++ calculator_for_buyers.js
@@ -9,14 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const selectedItem = form.item-select.value;
         const quantity = parseInt(form.quantity.value);
-        const akomericPrice = parseFloat(form.item-select.options[form.item-select.selectedIndex].dataset.price);
+        const sellRate = parseFloat(form.item-select.options[form.item-select.selectedIndex].dataset.price);
 
         if (selectedItem && !isNaN(quantity) && quantity > 0) {
-            // Calculate total price in gold
-            const totalPriceGold = (quantity / akomericPrice).toFixed(2);
+            // Calculate total price in gold based on the sell rate
+            const totalPriceGold = (quantity * sellRate).toFixed(2);
 
             // Display the result
-            resultDiv.innerHTML = `Total Price for ${quantity} ${selectedItem} is: 🪙 ${totalPriceGold}`;
+            resultDiv.innerHTML = `Total Price for ${quantity} ${selectedItem}(s) is: 🪙 ${totalPriceGold}`;
         } else {
             resultDiv.innerHTML = "Please select an item and enter a valid quantity.";
         }
